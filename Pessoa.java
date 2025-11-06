d2ba730309f2bd1b715798a108c59ac8fb178721
RaquelSilvino <raquel.silvinoalunounip@gmail.com>
2025-11-05 21:44:39 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,51 @@
+package com.objeto.app;
+
+import java.util.Scanner;
+public class Pessoa {
+    // atributos
+    public String nome;
+    public String cpf;
+    public String email;
+    public String idade;
+    public String altura;
+
+    // método para cadastrar pessoa
+    public "class",App{
+    public static void ain (String[]args) throws Exception {
+        System.out.printin("Informe o nome");
+        usuario.com= leia.nexline();
+        System.out.printin("Informe o CPF");
+        usuario.cpf= leia.nextline ();
+        System.out.printin("Informe o email");
+        usuario email= leia.nextline();
+        System.out.printin("Informe a idade");
+        usuario.idade=leia.nextInt();
+        System.out.printin("Informe a altura em metros");
+        usuario altura= leia.nextDouble();
+
+        // saida de dados
+        System.out.printin("Nome=" + usuario.nome):
+        System.out.printin("Nome=" + usuario.cpf):
+        System.out.printin("Nome=" + usuario.email):
+        System.out.printin("Nome=" + usuario.idade):
+        System.out.printin("Nome=" + usuario.altura):
+       usuario.exibirDados();
+
+
+        
+
+
+        
+
+    }
+    
+
+    // método para exibir dados da pessoa
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Email: " + email);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
