52c95bbde534f60a1bfc6f1d06907429b8af3060
Raquel.alvestec <raquel.silvinoalunounip@gmail.com>
2025-11-13 19:56:57 -0300

    Add files via upload

---
+++ public class App.java
@@ -0,0 +1,31 @@
+public class App
+
+import javax.swuing.JOptionPane;
+
+public class App{
+    Run/ Debong
+    public static void main(String[]args)throwa Exeption{
+        // declaração de variáveis
+        String nome;
+        String resultado;
+        Int idade;
+
+        entrada de dados
+        name=JOption.show.ImputDialog("Inform seu nome:");
+
+        // saide de dados
+        nome=JOptionPane.showMessageDialog("Seu nome é "+nome+".");
+        idade=Ineger.porseInt(JOptionPane.showImputDialog(("Sua idade é "+idade+"."));
+
+        // saida de dadps
+        JOptionPane.showmessageDialog(null,"Seu nome é"+nome+".");
+    }
+    else {
+        JOption.showMessageDialog(null, nome+"é menor de idade.");
+        // verifica idade
+        resultado=(idade>=18)? "é menor de idade";
+
+     // saida de dadps
+     JOptonPane.showmessageDialog(null, name+"resultado+".");
+     ")
+
